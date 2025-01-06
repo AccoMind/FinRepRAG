814fbe5b1a76f9778b0d75a7bbe6cf75e407fa67
Akmal <jasminaaa.20@uom.lk>
2025-01-06 15:14:26 +0530

    Update main.py to convert PDF to Markdown and add PDF to .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+from docling.document_converter import DocumentConverter
+
+source = "2408.09869v5-5.pdf"  # PDF path or URL
+converter = DocumentConverter()
+result = converter.convert(source)
+print(result.document.export_to_markdown())  # output: "### Docling Technical Report[...]"

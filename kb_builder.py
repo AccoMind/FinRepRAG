7fb074dfd32999325fcb90582282ab7673546cb7
Akmal <jasminaaa.20@uom.lk>
2025-02-01 22:04:36 +0530

    Add langchain support and update dependencies in kb_builder.py and requirements.txt

--- kb_builder.py
+++ kb_builder.py
@@ -4,7 +4,6 @@ from typing import List, Dict
 import re
 from datetime import datetime
 
-from google.colab import drive
 from dotenv import load_dotenv
 import pandas as pd
 from langchain_core.prompts import PromptTemplate
@@ -12,5 +11,5 @@ from langchain_docling.loader import DoclingLoader, ExportType
 from langchain_docling import DoclingLoader
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain.text_splitter import MarkdownHeaderTextSplitter
-from langchain_community.vectorstores import Milvus
+from langchain_milvus import Milvus
 from docling.chunking import HybridChunker

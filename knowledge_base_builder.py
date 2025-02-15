6387bce2cf1151ce26be5cb2307ea8f28cdd6d72
Akmal <jasminaaa.20@uom.lk>
2025-02-15 23:30:01 +0530

    Complete initializer

--- knowledge_base_builder.py
+++ knowledge_base_builder.py
@@ -1,17 +1,41 @@
-import os
 from pathlib import Path
-from typing import List, Dict
-import re
-from datetime import datetime
-
-from langchain_docling.loader import DoclingLoader, ExportType
-from langchain_huggingface import HuggingFaceEmbeddings
+from typing import List, Dict, Optional
+from langchain_core.embeddings import Embeddings
+from docling.document_converter import DocumentConverter
 from langchain_milvus import Milvus
-from docling.chunking import HybridChunker
 
 class KnowledgeBaseBuilder:
-    def __init__(self):
-        pass
+    def __init__(self, 
+                 folder_path: str,
+                 milvus_uri: str,
+                 milvus_user: str, 
+                 milvus_password: str,
+                 embedding_model: Embeddings,
+                 collection_name: str,
+                 doc_converter: Optional[DocumentConverter] = None):
+        self.folder_path = folder_path
+        self.milvus_uri = milvus_uri
+        self.milvus_user = milvus_user
+        self.milvus_password = milvus_password
+        self.embedding_model = embedding_model
+        self.collection_name = collection_name
+        self.doc_converter = doc_converter
+
+        # mount google drive only if it's colab
+        if 'google.colab' in str(get_ipython()): # type: ignore
+            from google.colab import drive # type: ignore
+            drive.mount('/content/drive', force_remount=True)
+
+        self.history_file = Path(folder_path) / "processing_history.json"
+        self.processed_files = self._load_processing_history()
+
+        self.vectorstore = self._initialize_vectorstore()
+        self.connection_args = {
+            "uri": self.milvus_uri, 
+            "user": self.milvus_user, 
+            "password": self.milvus_password,
+            "secure": True
+        }
 
     def _initialize_vectorstore(self):
         pass
